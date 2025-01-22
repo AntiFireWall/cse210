377f431713ef815c8f8200d3e6af9ab23417de48
BuildTools <unconfigured@null.spigotmc.org>
2025-01-22 20:39:48 +0400

    Week 2 Learning Activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using Fractions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(1,7);
+        Console.WriteLine(fraction1.GetTop());
+        Console.WriteLine(fraction1.GetBottom());
+        fraction2.SetTop(10);
+        Console.WriteLine(fraction2.GetTop());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
     }
 }
