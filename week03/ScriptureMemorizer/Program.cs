b489a208561d3bdc5269952d38dd03b265e90639
BuildTools <unconfigured@null.spigotmc.org>
2025-01-27 18:52:01 +0400

    Typo

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        // I add the ability to choose a verse from an api I found online (This tool HOURSE!!!!).
+        // I add the ability to choose a verse from an api I found online (This took HOURSE!!!!).
         // I also made the program hide words that are not hidden, meaning it will not attempt to hide hidden words.
         
         await ConsoleInterface.InterfaceWithApi();
